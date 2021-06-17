f5ff5c730647ab24613360f759d86f5ef69877ce
sonhle9 <sonhle9@gmail.com>
2021-06-17 21:01:53 +0700

    remove add case for extraReducers

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -15,30 +15,22 @@ export const fetchUsers = createAsyncThunk('user/fetchCount', async () => {
 export const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {
-    decrement: (state) => {
-      state.value -= 1;
+  extraReducers: {
+    [fetchUsers.pending]: (state) => {
+      state.loading = true;
     },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchUsers.pending, (state) => {
-        state.loading = true;
-      })
-      .addCase(fetchUsers.fulfilled, (state, action) => {
-        state.loading = false;
-        state.users = action.payload.user;
-        state.error = '';
-      })
-      .addCase(fetchUsers.fulfilled, (state, action) => {
-        state.loading = false;
-        state.users = [];
-        state.error = action.payload;
-      });
-  },
-});
-
-export const { decrement } = userSlice.actions;
+    [fetchUsers.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.users = action.payload.user;
+      state.error = '';
+    },
+    [fetchUsers.rejected]: (state, action) => {
+      state.loading = false;
+      state.users = [];
+      state.error = action.payload;
+    },
+  }
+})
 
 export const selectCurrentUser = (state) => state.user.users
 
