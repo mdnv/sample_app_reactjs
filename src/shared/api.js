631f30e33b1dc580b608f17a3972387ebf2e4b54
Sơn Hoàng <sonhle9@gmail.com>
2021-06-26 18:41:48 +0700

    Update api.js

--- src/shared/api.js
+++ src/shared/api.js
@@ -14,7 +14,7 @@ axios.defaults.xsrfHeaderName = 'X-CSRF-Token';
 axios.defaults.withCredentials = true;
 
 const getCookie = (name) => {
-    let match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
     if (match) return match[2];
 }
 
